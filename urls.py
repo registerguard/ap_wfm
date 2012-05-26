7f357e4bf2ba87a47871c8e3669cf9108cc399f1
J. Heasly <jheasly@earthlink.net>
2012-05-26 11:49:49 -0700

    Got list view working.

--- urls.py
+++ urls.py
@@ -1,13 +1,7 @@
-import datetime
 from django.conf.urls.defaults import *
-from django.views.generic import ListView
-from ap_wfm.models import APStory
-from ap_wfm.views import APStoryDetailView
+from ap_wfm.views import APStoryListView, APStoryDetailView
 
 urlpatterns = patterns('',
-    (r'^$', ListView.as_view(
-        model = APStory,
-        queryset = APStory.objects.filter(published__lte=datetime.datetime.now(), consumer_ready=True),
-    )),
+    (r'^$', APStoryListView.as_view()),
     (r'^(?P<pk>\d+)$', APStoryDetailView.as_view()),
 )
--- views.py
+++ views.py
@@ -1,5 +1,10 @@
-from django.views.generic import DetailView
+import datetime
+from django.views.generic import DetailView, ListView
 from ap_wfm.models import APStory
+
+class APStoryListView(ListView):
+    def get_queryset(self):
+        return APStory.objects.filter(published__lte=datetime.datetime.now(), consumer_ready=True)
 
 class APStoryDetailView(DetailView):
     model = APStory
