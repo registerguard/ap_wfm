b0ec0d613e4dad9cb40d7befc83419d29ed3a58f
J. Heasly <jheasly@earthlink.net>
2014-06-12 19:02:37 -0700

    More doc strings.

--- call_process_feed_example.py
+++ call_process_feed_example.py
@@ -3,7 +3,38 @@ def main():
     import os, sys
     from os import environ
     '''
-    Adds parent of call_process_feed.py to sys.path:
+    The first sys.path.append adds the parent directory of call_process_feed.py 
+    to sys.path.
+    The second sys.path.append bit adds the path to your Django project 
+    settings.
+    These two sys.path.append examples assume your Django project directory 
+    is next to your ap_wfm directory. Like this:
+    
+    ├── ap_wfm
+    │   ├── README.md
+    │   ├── WFA
+    │   │   ├── config
+    │   │   │   └── configuration.xml
+    │   │   ├── feeds
+    │   │   │   ├── OneOfYourFeeds
+    │   │   │   │   ├── feed_646541_2014-06-12T00-51-22.558Z.xml
+    │   │   │   │   ├── feed_646541_2014-06-12T01-12-34.622Z.xml
+    │   │   │   │   ├── feed_646541_2014-06-12T01-33-38.907Z.xml
+    │   │   │   │   ├── feed_646541_2014-06-12T23-08-39.370Z.xml
+    │   │   │   │   ├── feed_646541_2014-06-12T23-14-02.723Z.xml
+    
+        [ ... ]
+    
+    │   │   └── logs
+    │   │       ├── WebFeedsAgent.log
+    │   │       └── WebFeedsAgent.log.2014-06-11.1
+    ├── your_project_dir
+    │   ├── README.rst
+    │   ├── __init__.py
+    │   ├── ap_wfm -> ../ap_wfm    
+    
+    Your may need to adjust the number of relative path moves '..' to suit 
+    your particular layout.
     
     '''
     sys.path.append(os.path.join(os.path.abspath(os.path.dirname(__file__)),
