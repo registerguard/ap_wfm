7e025c50fa3176dffcacb1e1b383cb6776a3e903
J. Heasly <jheasly@earthlink.net>
2017-03-10 15:11:23 -0800

    Stronger temp patch.

--- models.py
+++ models.py
@@ -89,7 +89,10 @@ class APStory(models.Model):
         return '%s ID:%s' % (self.headline, self.id)
 
     def get_absolute_url(self):
-        return '/apf/%s/%s/' % (self.category.all()[0].get_name_display(), self.slug)
+        try:
+            return '/apf/%s/%s/' % (self.category.all()[0].get_name_display(), self.slug)
+        except IndexError:
+            return '/apf/%s/%s/' % ('us', self.slug)
 
     def image_count(self):
         return self.image_set.count()
