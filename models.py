18dd0bf76a11d3a9831fb302833b4f240cb1ac80
John Heasly <jheasly@earthlink.net>
2018-02-27 13:07:35 -0800

    remove cuddlybuddly S3Error

--- models.py
+++ models.py
@@ -4,7 +4,6 @@ from django.db import models
 from django.http import HttpResponse
 from django.utils import simplejson
 from ap_wfm.templatetags.humanize_list import humanize_list
-from cuddlybuddly.storage.s3.storage import S3Error
 from sorl.thumbnail import ImageField, get_thumbnail
 
 def json_response(func):
@@ -128,16 +127,9 @@ class Image(models.Model):
         return self.original_filename
 
     def to_json_image_dict(self):
-        try:
-            json_image = get_thumbnail(self.image, '990x990')
-            return {
-                'description': self.caption,
-                'byline': self.source,
-                'image': json_image.url
-            }
-        except (S3Error, IOError, TypeError):
-            return {
-                'description': self.caption,
-                'byline': self.source,
-                'image': ''
-            }
+        json_image = get_thumbnail(self.image, '990x990')
+        return {
+            'description': self.caption,
+            'byline': self.source,
+            'image': json_image.url
+        }
